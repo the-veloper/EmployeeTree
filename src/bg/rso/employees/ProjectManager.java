2a63418b77b816c3e1f44b8c84c889fe379f66bf
Georgi Georgiev <g.georgiev@rso.bg>
2018-11-06 08:46:32 +0200

    PM Java class

---
+++ src/bg/rso/employees/ProjectManager.java
@@ -0,0 +1,26 @@
+package bg.rso.employees;
+
+import bg.rso.departments.Department;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectManager extends Employee {
+    List<Employee> managingEmployees = new ArrayList<Employee>();
+
+    public ProjectManager(long employeeId, String employeeName, String designation, Department department) {
+        super(employeeId, employeeName, designation, department);
+    }
+
+    public boolean manages(Employee employee) {
+        return managingEmployees.add(employee);
+    }
+
+    public boolean stopManaging(Employee employee) {
+        return managingEmployees.remove(employee);
+    }
+
+    @Override
+    public int teamSize() {
+        return managingEmployees.stream().mapToInt(employee -> employee.teamSize()).sum();
+    }
+}
